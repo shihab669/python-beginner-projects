fe820dc0d8c551b5581e92208d8eb0d3e7831faa
Shihab Uddin <shihabyt06@gmail.com>
2026-01-15 12:06:44 +0600

    made some changes

--- currency_converter.py
+++ currency_converter.py
@@ -1,7 +1,7 @@
 import requests
 
-API = "https://v6.exchangerate-api.com/v6/2d859472b3eb018f5f2281db/latest/USD"
+API = "https://v6.exchangerate-api.com/v6/APIKEY/latest/USD"
 
 response = requests.get(API)
 
-# Will work later 
+# Will work later on implementing the currency conversion logic
