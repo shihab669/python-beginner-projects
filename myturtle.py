3aa424d5d1be879fd12352359bbeb2a607ba79db
Shihab Uddin <shihabyt06@gmail.com>
2026-01-22 22:57:11 +0600

    changed file name

---
+++ myturtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+t = turtle.Turtle()
+
+# Example drawing: A square
+for _ in range(4):
+    t.forward(100)  # Move forward by 100 units
+    t.right(90)     # Turn right by 90 degrees
+
+turtle.done()
