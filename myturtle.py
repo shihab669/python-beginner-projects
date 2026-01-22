a710282d9cc876f95ed843a863c744777b35d864
Shihab Uddin <shihabyt06@gmail.com>
2026-01-22 23:09:57 +0600

    finished adding myturtle

--- myturtle.py
+++ myturtle.py
@@ -1,10 +1,24 @@
 import turtle
 
 t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.pencolor("green")
 
-# Example drawing: A square
-for _ in range(4):
-    t.forward(100)  # Move forward by 100 units
-    t.right(90)     # Turn right by 90 degrees
+a = 0
+b = 0
+t.penup()
+t.goto(0, 0)
+t.pendown()
+t.speed(0)
+
+while True:
+    t.forward(a)
+    t.left(b)
+    a += 3
+    b += 1
+    if b == 210:
+        break
+    t.hideturtle()
 
 turtle.done()
